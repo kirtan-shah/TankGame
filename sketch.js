b62c91296c98a4e8131855e4a0d1dd55688eb3be
kirtan-shah <kirtanushah@gmail.com>
2018-03-15 14:43:52 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,43 @@
 
+
+var x = 0;
+var y = 0;
+
+var up, down, left, right;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
 }
 
+void keyPressed() {
+  if(keyCode == 38)
+    up = true;
+  if(keyCode == 40)
+    down = true;
+  if(keyCode == 37)
+    left = true;
+  if(keyCode == 39)
+    right = true;
+}
+
+void keyReleased() {
+  if(keyCode == 38)
+    up = true;
+  if(keyCode == 40)
+    down = true;
+  if(keyCode == 37)
+    left = true;
+  if(keyCode == 39)
+    right = true;
+}
+
 function draw() {
   background(0, 220, 160);
+  if(up) y--;
+  if(down) y++;
+  if(left) x--;
+  if(right) x++;
+  fill(0, 180, 30);
+  rect(x, y, 50, 50);
 }
