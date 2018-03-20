1cde85a87b9c41951c1d176e65df37851b3d76c2
kirtan-shah <kirtanushah@gmail.com>
2018-03-20 15:10:02 -0700

    Create gui.js

---
+++ gui.js
@@ -0,0 +1,15 @@
+
+var mk3 = $("<button class="tab">Mark III</button>");
+mk3.css({
+"position" : "absolute",
+"top" : 0,
+"left" : 0,
+"width" : "60px"
+});
+var t34 = $("<button class="tab">T-34</button>");
+t34.css({
+"position" : "absolute",
+"top" : "60px,
+"width" : "60px"
+});
+
